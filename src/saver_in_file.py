56dcb9a94acce929c6a481619d365ea1e4455427
glebk <glebkot57@gmail.com>
2023-07-05 01:42:00 +0200

    add docstrings

--- src/saver_in_file.py
+++ src/saver_in_file.py
@@ -5,29 +5,49 @@ import csv
 
 
 class Saver_file(ABC):
-
+    """
+        Абстрактный базовый класс для сохранения данных.
+    """
     @abstractmethod
     def jobs_adding(self, jobs):
+        """
+            Абстрактный метод для добавления вакансий.
+        """
         pass
 
     @abstractmethod
     def get_salary(self, salary):
+        """
+            Абстрактный метод для получения вакансий по зарплате.
+        """
         pass
 
     @abstractmethod
     def get_city(self, citi):
+        """
+            Абстрактный метод для получения вакансий по городу.
+        """
         pass
 
     @abstractmethod
     def get_experience(self, experience):
+        """
+            Абстрактный метод для получения вакансий по опыту работы.
+        """
         pass
 
     @abstractmethod
     def jobs_deleting(self, jobs):
+        """
+            Абстрактный метод для удаления вакансий.
+        """
         pass
 
     @abstractmethod
     def file_cleaning(self):
+        """
+            Абстрактный метод для очистки файла..
+        """
         pass
 
 
@@ -35,6 +55,9 @@ class Json_Saver(Saver_file):
     file = os.path.join("saver file", 'vacancy.json')
 
     def jobs_adding(self, jobs: dict) -> None:
+        """
+            Метод для добавления вакансий в файл JSON.
+        """
         if not os.path.exists(self.file):
             with open(self.file, "w", encoding="utf-8") as file:
                 file.write("[]")
@@ -50,24 +73,36 @@ class Json_Saver(Saver_file):
                                    escape_forward_slashes=False))
 
     def get_salary(self, salary: int) -> list:
+        """
+            Метод для получения вакансий по зарплате.
+        """
         with open(self.file, 'r', encoding="utf-8") as file:
             ujson_data = ujson.load(file)
             return [ujson_data[item] for item in range(len(ujson_data)) if
                     ujson_data[item].get('_Vacancy__salary') == salary]
 
     def get_city(self, city: str) -> list:
+        """
+            Метод для получения вакансий по городу.
+        """
         with open(self.file, 'r', encoding="utf-8") as file:
             ujson_data = ujson.load(file)
             return [ujson_data[item] for item in
                     range(len(ujson_data)) if ujson_data[item]['_Vacancy__city'] == city]
 
     def get_experience(self, experience):
+        """
+            Метод для получения вакансий по опыту работы.
+        """
         with open(self.file, 'r', encoding="utf-8") as file:
             ujson_data = ujson.load(file)
             return [ujson_data[item] for item in range(len(ujson_data)) if
                     ujson_data[item]['_Vacancy__experience'] == experience]
 
     def jobs_deleting(self, jobs):
+        """
+            Метод для удаления вакансий.
+        """
         try:
             ujson_data = ujson.load(open(self.file))
             ujson_data.remove(jobs)
@@ -79,19 +114,22 @@ class Json_Saver(Saver_file):
             print('Все вакансии удалены')
 
     def file_cleaning(self):
+        """
+            Метод для очистки файла.
+        """
         os.remove(self.file)
 
-json_saver = Json_Saver()
-
-job1 = {
-    'name': 'Software Engineer',
-    'salary': 5000,
-    'experience': '3 years',
-    'city': 'New York',
-    'url': 'example.com',
-    'requirement': 'Requirements'
-}
-json_saver.jobs_adding(job1)
-
-salary_5000 = json_saver.get_salary(5000)
-print(salary_5000)
+# json_saver = Json_Saver()
+#
+# job1 = {
+#     'name': 'Software Engineer',
+#     'salary': 5000,
+#     'experience': '3 years',
+#     'city': 'New York',
+#     'url': 'example.com',
+#     'requirement': 'Requirements'
+# }
+# json_saver.jobs_adding(job1)
+#
+# salary_5000 = json_saver.get_salary(5000)
+# print(salary_5000)
